2eeffc2bd33e74d01680af7610eeda0b4e778901
Wojciech Antonik <antonik.wojciech2000@gmail.com>
2020-06-02 17:04:04 +0200

    setup of project

---
+++ server.js
@@ -0,0 +1,14 @@
+const express = require('express')
+const app = express()
+const mongoose = require('mongoose')
+const cors = require('cors')
+
+const PORT = process.env.PORT || 5000
+
+app.use(cors())
+app.use(express.json())
+app.use(express.urlencoded({ extended: false }))
+
+mongoose.connect(process.env.MONGODB_URI || 'mongodb+srv://wojciech:start179@cluster0-q41na.mongodb.net/test?retryWrites=true&w=majority', { useNewUrlParser: true, useUnifiedTopology: true }, ()=> console.log('connected with DB'))
+
+app.listen(PORT, () => console.log(`server running on port ${PORT}`))
