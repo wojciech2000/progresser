340d7dc878c3e9ccf2c2377c18b4e317986a4648
Wojciech Antonik <antonik.wojciech2000@gmail.com>
2020-06-02 17:28:06 +0200

    set process.env.NODE_ENV

--- server.js
+++ server.js
@@ -12,4 +12,13 @@ app.use(express.urlencoded({ extended: false }))
 
 mongoose.connect(process.env.MONGODB_URI || process.env.MONGODB_LOCAL, { useNewUrlParser: true, useUnifiedTopology: true }, ()=> console.log('connected with DB'))
 
+if(process.env.NODE_ENV === 'production')
+{
+    app.use(express.static('client/build'))
+    app.get('*', (req,res)=> {
+        res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'))
+    })
+    
+}
+
 app.listen(PORT, () => console.log(`server is running on port ${PORT}`))
