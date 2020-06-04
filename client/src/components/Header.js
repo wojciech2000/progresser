8fe7bc070e6bead10cac0860e36ef521b211e9bc
Wojciech Antonik <antonik.wojciech2000@gmail.com>
2020-06-04 14:18:21 +0200

    create Header.js

---
+++ client/src/components/Header.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function Header() {
+    return (
+        <div className="header">
+            <h1 className="header__h1">Pro<span className="header__cut-on-mobile">gresser</span></h1>
+        </div>
+    )
+}
+
+export default Header
