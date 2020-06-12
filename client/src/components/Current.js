c4b3e320ec1c16dba6ca0ded53594d8f68f6b905
Wojciech Antonik <antonik.wojciech2000@gmail.com>
2020-06-12 09:28:38 +0200

    create message in Current.js if there is no data

--- client/src/components/Current.js
+++ client/src/components/Current.js
@@ -1,5 +1,6 @@
 import React from 'react'
 import { useSelector } from 'react-redux'
+import boarIllustration from '../pictures/black-boar.png'
 
 function Current() {
     
@@ -10,14 +11,23 @@ function Current() {
     {
         // change object of data(except id and date) into arry so it be possible to use array methods
         const arrayDatas = Object.entries(currentData).slice(2, Object.entries(currentData).length)
-        console.log()
         return(
             arrayDatas.map((data, id) => {
                 if(data[0] !== 'image' && dataType === 'span') return <span key={id}>{data[0]}: {data[1]}cm</span>
                 else if(data[0] === 'image' && dataType === 'image') return <img key={id} className="current__image" alt="zdjęcie sylwetki" src={document.location.origin + '/' + data[1]} />
             })
         )
-    }}
+    }
+    else if(!currentData && dataType==='span')
+    {
+        return (
+            <div className="current__no-data">
+                <span className="no-data__span">dodaj dane, dziku</span>
+                <img className="no-data__image" src={boarIllustration} alt="boar-illustration"/>
+            </div>)
+    }
+}
+
 
     return (
         <div className="current">
