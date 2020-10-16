a19a624046e9e8d29a7bd6e1c3705eda024a6966
Wojciech Antonik <wojtekantonik9@gmail.com>
2020-10-16 17:19:42 +0200

    change animation of onTap data's name to framer motion

--- client/src/components/AddData.js
+++ client/src/components/AddData.js
@@ -1,52 +1,39 @@
-import React, { useContext, useState } from "react";
-import { DataContext } from "./DataContext";
-import { useSelector } from "react-redux";
-import { FaArrowAltCircleRight } from "react-icons/fa";
-import { motion } from "framer-motion";
-import { TweenMax, Power4, Back } from "gsap";
+import React, {useContext, useState} from "react";
+import {DataContext} from "./DataContext";
+import {useSelector} from "react-redux";
+import {FaArrowAltCircleRight} from "react-icons/fa";
+import {motion} from "framer-motion";
 
 function AddData(props) {
   const dataNames = useSelector(state => state.dataNames);
-  const { loggedMessage, pageVariants, pageTransition } = useContext(
-    DataContext
-  );
+  const {loggedMessage, pageVariants, pageTransition} = useContext(DataContext);
 
   const [choosenData, setChoosenData] = useState([]);
 
-  const mouseDown = e => {
-    TweenMax.to(e.target, { scale: 0.8, duration: 0.2, ease: Power4.easeOut });
-  };
-
-  const mouseUp = e => {
+  const addDataName = e => {
     const dataName = e.target;
     dataName.classList.toggle("select-data__data-name--active");
 
     const isInArray = choosenData.find(
-      current => current === dataName.textContent
+      current => current === dataName.textContent,
     );
 
     if (isInArray) {
       //remove from array
       setChoosenData(
         choosenData.filter(
-          current => current !== dataName.textContent && current
-        )
+          current => current !== dataName.textContent && current,
+        ),
       );
     } else {
       //add to array
       setChoosenData([...choosenData, dataName.textContent]);
     }
-
-    TweenMax.to(e.target, {
-      scale: 1,
-      duration: 0.5,
-      ease: Back.easeOut.config(6),
-    });
   };
 
   const confirm = () => {
     const chosenData = Array.from(
-      document.querySelectorAll(".select-data__data-name--active")
+      document.querySelectorAll(".select-data__data-name--active"),
     );
 
     if (chosenData.length === 0)
@@ -61,24 +48,27 @@ function AddData(props) {
 
   return (
     <motion.div
-      className='add-data'
-      initial='in'
-      animate='done'
-      exit='out'
+      className="add-data"
+      initial="in"
+      animate="done"
+      exit="out"
       variants={pageVariants}
-      transition={pageTransition}>
-      <div className='select-data'>
+      transition={pageTransition}
+    >
+      <div className="select-data">
         {dataNames.map((name, id) => (
-          <div
-            onMouseDown={mouseDown}
-            onMouseUp={mouseUp}
-            className='select-data__data-name'
-            key={id}>
+          <motion.div
+            onClick={e => addDataName(e)}
+            className="select-data__data-name"
+            key={id}
+            whileTap={{scale: 0.85}}
+            transition={{type: "spring", stiffness: 400}}
+          >
             {name === "image" ? "zdjęcie" : name}
-          </div>
+          </motion.div>
         ))}
 
-        <button className='select-data__confirm' onClick={confirm}>
+        <button className="select-data__confirm" onClick={confirm}>
           <FaArrowAltCircleRight />
         </button>
       </div>
