cf2008e3fad6feb1246ec96b8270c632282a9fe4
unknown <wojtekantonik9@gmail.com>
2020-12-01 16:54:30 +0100

    fix overflow: hidden on mobile

--- client/src/App.js
+++ client/src/App.js
@@ -1,14 +1,16 @@
-import React, { Fragment } from "react";
+import React, {Fragment} from "react";
 import Header from "./components/Header";
 import Body from "./components/Body";
-import { DataProvider } from "./components/DataContext";
+import {DataProvider} from "./components/DataContext";
 
 function App() {
   return (
     <Fragment>
       <DataProvider>
-        <Header />
-        <Body />
+        <div className="wrapper">
+          <Header />
+          <Body />
+        </div>
       </DataProvider>
     </Fragment>
   );
